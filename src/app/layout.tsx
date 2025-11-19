5c26a29ca63ad39a6cdf5580a5d5a3f881ef4dc0
Deepu Kumar Das <dk4993722@gmail.com>
2025-11-19 17:46:55 +0000

    only change background color sky blue

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased bg-gradient-to-t from-background to-primary">
+      <body className="font-body antialiased">
         {children}
         <Toaster />
       </body>
