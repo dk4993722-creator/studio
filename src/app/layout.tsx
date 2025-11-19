e8df1783ccc9f16dbb4a7bc14c3b0458e18fcff2
Deepu Kumar Das <dk4993722@gmail.com>
2025-11-19 17:48:31 +0000

    change back ground color like

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,13 +13,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased bg-gradient-to-b from-[#240051] via-[#3d0f73] to-[#c71f66]">
         {children}
         <Toaster />
       </body>
